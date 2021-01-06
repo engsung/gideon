d9842f0cf9630972fbcd848410b966856e875d34
Eng Sung <engx03@gmail.com>
2021-01-06 18:00:05 -0500

    loading data issue resolved

--- digit_recognition_tf/digit_recog2.py
+++ digit_recognition_tf/digit_recog2.py
@@ -2,6 +2,8 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+
+# problems with loading data with tf, issue resolved with this
 import ssl
 try:
     _create_unverified_https_context = ssl._create_unverified_context
